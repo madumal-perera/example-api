5bed762990600c9d1e7e27bce6307245754ae885
Madumal Perera <madumalbiz18@gmail.com>
2021-07-28 17:10:04 +0530

    main route setup

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+  res.status(200).send('Welcome ExpressJS!')
+})
+
 app.get('/api/v1/posts', (req, res) => {
   res.status(200).json({
     status: 'success',
